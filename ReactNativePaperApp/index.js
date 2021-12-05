f6f7b031752468aa24facfc16d199e1e49bdebb6
Omar Magdy <omar.magdy.code@gmail.com>
2021-12-05 11:07:34 +0200

    React Native up and running

--- ReactNativePaperApp/index.js
+++ ReactNativePaperApp/index.js
@@ -1,9 +1,43 @@
 /**
  * @format
  */
-
+/*
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+*/
+
+
+import * as React from 'react';
+import { AppRegistry } from 'react-native';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+
+import { name as appName } from './app.json';
+import App from './App';
+
+
+
+const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: 'tomato',
+      accent: 'yellow',
+    },
+  };
+
+
+export default function Main() {
+  return (
+    <PaperProvider theme={theme}>
+      <App />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
+
+
